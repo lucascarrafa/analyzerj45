a3dd64aed99753865e244249509ec8183e10a6ff
Lucas Carrafa <lucascarrafa@hotmail.com>
2019-03-17 13:32:00 -0300

    add data e hora

--- teste.py
+++ teste.py
@@ -1,7 +1,9 @@
+import datetime
 from scapy.all import *
 
 def capture(dado):
 	with open("Output.txt", "a") as text_file:
-	    text_file.write(str(dado.sprintf("{IP:%IP.src% %IP.dst% %IP.len% %IP.proto%\n}")))
+	    import datetime
+	    text_file.write(str(datetime.datetime.now())+" "+str(dado.sprintf("{IP:%IP.src% %IP.dst% %IP.len% %IP.proto%\n}")))
 
-sniff(iface="eth0", prn=capture, timeout=60)
+sniff(iface="eth0", prn=capture, timeout=10)
