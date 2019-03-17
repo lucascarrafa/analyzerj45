0f4d18c98baf108feddbd275b3ba3bbf6e807b88
Lucas Carrafa <lucascarrafa@hotmail.com>
2019-03-17 13:09:57 -0300

    coleta

--- teste.py
+++ teste.py
@@ -4,4 +4,4 @@ def capture(dado):
 	with open("Output.txt", "a") as text_file:
 	    text_file.write(str(dado.sprintf("{IP:%IP.src% %IP.dst% %IP.len% %IP.proto%\n}")))
 
-sniff(iface="eth0", prn=capture, count=100)
+sniff(iface="eth0", prn=capture, timeout=60)
