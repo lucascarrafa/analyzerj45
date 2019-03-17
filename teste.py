daea0d42617e4025f4529fa7a0023bafecec985b
Lucas Carrafa <lucascarrafa@hotmail.com>
2019-03-17 12:35:01 -0300

    add proto

--- teste.py
+++ teste.py
@@ -2,7 +2,6 @@ from scapy.all import *
 
 def capture(dado):
 	with open("Output.txt", "a") as text_file:
-	    text_file.write(str(dado.sprintf("{IP:%IP.src% %IP.dst% %IP.len%\n}")))
-	    #text_file.write(str(dado.sniffed_on+": "+dado.summary()+"\n"))
+	    text_file.write(str(dado.sprintf("{IP:%IP.src% %IP.dst% %IP.len% %IP.proto%\n}")))
 
 sniff(iface="eth0", prn=capture, count=100)
